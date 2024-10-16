7a842aa1eb08837ecb1cdfbd123d410a6323194c
isaac <minimaleffort769@gmail.com>
2024-10-16 21:24:41 +0700

    chore(frontend,spa): initial root page

--- frontend/spa/src/main.tsx
+++ frontend/spa/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+
+import App from './App';
+
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={createBrowserRouter([
+      {
+        path: "/",
+        element: <App />,
+        children: []
+      }
+    ])} />
   </StrictMode>,
 )
