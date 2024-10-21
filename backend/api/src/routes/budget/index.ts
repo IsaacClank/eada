4a449964f0842a8424c6496a3059cdd63a1f9ea2
isaac <minimaleffort769@gmail.com>
2024-10-21 21:29:07 +0700

    feat(backend,api): budget retrieval endpoint

--- backend/api/src/routes/budget/index.ts
+++ backend/api/src/routes/budget/index.ts
@@ -57,6 +57,39 @@ const routes: FastifyPluginAsyncTypebox = async function (app) {
       });
     },
   );
+
+  app.get(
+    "/",
+    {
+      onRequest: [app.authorize()],
+      schema: {
+        response: {
+          200: Type.Object({
+            name: Type.String(),
+            normalizedName: Type.String(),
+            income: Type.Number(),
+          }),
+        },
+      },
+    },
+    async (req, res) => {
+      const { sub: userId } = req.user;
+      const budget = await db.budget.findFirst({
+        where: { userId },
+      });
+
+      if (budget == null) {
+        res.notFound();
+        return;
+      }
+
+      res.send({
+        name: budget.name,
+        normalizedName: budget.normalizedName,
+        income: budget.income.toNumber(),
+      });
+    },
+  );
 };
 
 export default routes;
