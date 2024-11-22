c817ed192ed4ec4d9b4ca72b79683e33b548eb00
isaac <minimaleffort769@gmail.com>
2024-11-22 23:32:23 +0700

    chore(frontend,spa,tailwind): customized color palette

--- frontend/spa/tailwind.config.js
+++ frontend/spa/tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          50: "#29343D",
+          200: "#415362",
+        },
+        pink: {
+          600: "#DA6C8F",
+        },
+        yellow: {
+          600: "#F7BC50",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
