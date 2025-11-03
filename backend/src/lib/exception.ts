c485ec502498bb3e76cf8120715e98458c37d71e
Isaac <minimaleffort768@gmail.com>
2025-11-03 12:44:23 +0700

    chore(backend): re-usable exception class

--- backend/src/lib/exception.ts
+++ backend/src/lib/exception.ts
@@ -1,8 +1,15 @@
-import { ErrorStatus } from "@oak/common/status";
+import { ErrorStatus, Status } from "@oak/common/status";
 import { HttpError } from "@oak/common/http_errors";
 
 export class HttpException<T extends ErrorStatus> extends HttpError<T> {
   constructor(errorCode: string, cause?: string) {
     super(errorCode, { cause });
+  }
+}
+
+export class NotImplementedException
+  extends HttpException<Status.NotImplemented> {
+  constructor() {
+    super("NotImplemented");
   }
 }
